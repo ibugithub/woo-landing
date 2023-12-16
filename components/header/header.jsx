4a04f4371965a68bd6a52736246e237ca17db43b
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-12-16 09:11:47 +0600

    Remove default setup and add header components

---
+++ components/header/header.jsx
@@ -0,0 +1,26 @@
+const Header = () => {
+  return (
+    <div className="flex">
+    <div>
+      Header icon
+    </div>
+
+    <ul>
+      <li>Sell</li>
+      <li>MarketPlace</li>
+      <li>Community</li>
+      <li>Develop</li>
+      <li>Resource</li>
+    </ul>
+
+    <ul>
+      <li>Log In</li>
+      <li>Get Started</li>
+      <li>search icon</li>
+    </ul>
+    </div>
+    
+  );
+}
+
+export default Header;
