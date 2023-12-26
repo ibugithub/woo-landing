f96a006a2f19796cabab373fc7846c4fe3ae38e6
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-12-26 16:32:57 +0600

    Add the bottom section of feature section

--- components/feature-section/content/feature-items.js
+++ components/feature-section/content/feature-items.js
@@ -1,7 +1,7 @@
 const FeatureItems = [
   {img: "/images/feature1.png", title: "All You Need to Start", description: "Add WooComerce to any Wordpress site and set up a new store in a minute", linkTitle: "Ecommerce for Wordpress", link: ""},
   {img: "/images/feature2.png", title: "Customize and Extend", description: "From subscriptions to gym classes to luxury cars, WooCommerce is fully customizable.", linkTitle: "Browser Extension", link: ""},
-  {img: "/images/feature3.png", title: "Active Community", description: "From subscriptions to gym classes to luxury cars, WooCommerce is fully customizable.", linkTitle: "Check our Forums", link: ""},
+  {img: "/images/feature3.png", title: "Active Community", description: "WooCommerce is one of the fastest-growing eCommerce communities.", linkTitle: "Check our Forums", link: ""},
 ]
 
 export default FeatureItems;
