802b98aac9d6551d9d81cf672e6e1ecc96d578d3
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-12-26 08:30:23 +0600

    Added Hero section

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,25 +1,25 @@
 import Image from "next/image";
+import HeaderNav from "@/content/headerNabLink";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="flex  justify-between font-medium  p-6">
-      <div className="flex">
+      <div className="flex ">
         <Image
           src="/images/logo.png"
           width={170}
           height={30}
           alt="this is a logo"
         />
-        <ul className="flex gap-4 pl-20 items-center">
-          <li>Sell</li>
-          <li>MarketPlace</li>
-          <li>Community</li>
-          <li>Develop</li>
-          <li>Resource</li>
+        <ul className="flex items-center gap-4 pl-16">
+          {HeaderNav.map(({href, title}) => (
+            <Link key={title} href={href}>{title}</Link>
+          ))}
         </ul>
       </div>
 
-      <ul className="flex gap-4 items-center">
+      <ul className="flex gap-4 items-center ">
         <li>Log In</li>
         <li className="bg-[#7854F7] text-white px-3 py-2 rounded" >Get Started</li>
         <li>
