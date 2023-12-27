2fcd546fbce81859b149fdcf5b9dc9f5c9b26709
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-12-27 09:43:44 +0600

    Add Section page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import Header from "@/components/header/header"
 import Hero from "@/components/hero"
 import FeatureSection from "@/components/feature-section/feature-section"
+import Section from "@/components/Section/section"
+
 
 export default function Home() {
   return (
@@ -8,6 +10,7 @@ export default function Home() {
     <Header/>
     <Hero />
     <FeatureSection />
+    <Section />
     </>
   )
 }
